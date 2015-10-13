3f8fb210bc6cd85d6326dc69930fcf5d07417ee2
daniel-anderson-dev <daniel.anderson28562@hotmail.com>
2015-10-13 18:45:57 -0400

    Setup various configuration / startup Classes.

---
+++ MvcMovie/App_Start/Startup.Auth.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+
+namespace MvcMovie
+{
+	public partial class Startup
+	{
+		public void ConfigureAuth(IAppBuilder app)
+		{
+			app.UseCookieAuthentication(new CookieAuthenticationOptions
+			{
+				AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+				LoginPath = new PathString("/Account/Login")
+			});
+
+			app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+		}
+	}
+}
